ac61c82e0397de527c92694ea1114e6c9586cc51
avantika <namelyavantika@gmail.com>
2025-04-07 10:20:16 +0530

    feat: history table now displays time of transaction

--- public/js/firebase.js
+++ public/js/firebase.js
@@ -148,7 +148,7 @@ submitTransaction.addEventListener("click", async (event) => {
                 description: description,
                 type: type,
                 createdAt: new Date(),
-                date: Date.now(),
+                date: new Date(),
             });
             console.log("Doc added", docRef);
             document.getElementById("description").value = "";
@@ -166,6 +166,10 @@ submitTransaction.addEventListener("click", async (event) => {
 
 const historyTable = document.getElementById("history-table");
 const historyBody = document.getElementById("history-body");
+const deleteIcon = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash3-fill" viewBox="0 0 16 16">
+  <path d="M11 1.5v1h3.5a.5.5 0 0 1 0 1h-.538l-.853 10.66A2 2 0 0 1 11.115 16h-6.23a2 2 0 0 1-1.994-1.84L2.038 3.5H1.5a.5.5 0 0 1 0-1H5v-1A1.5 1.5 0 0 1 6.5 0h3A1.5 1.5 0 0 1 11 1.5m-5 0v1h4v-1a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5M4.5 5.029l.5 8.5a.5.5 0 1 0 .998-.06l-.5-8.5a.5.5 0 1 0-.998.06m6.53-.528a.5.5 0 0 0-.528.47l-.5 8.5a.5.5 0 0 0 .998.058l.5-8.5a.5.5 0 0 0-.47-.528M8 4.5a.5.5 0 0 0-.5.5v8.5a.5.5 0 0 0 1 0V5a.5.5 0 0 0-.5-.5"/>
+</svg>`;
+const tableClass = "text-center"
 
 export const getHistory = async () => {
     console.log("Inside the get history function")
@@ -177,14 +181,18 @@ export const getHistory = async () => {
         });
 
         if (doc.length == 0) {
+            historyBody.innerHTML = "";
             historyBody.innerHTML = "Nothing to show here."
         } else {
+            historyBody.innerHTML = "";
             querySnapshot.forEach((doc, index) => {
                 const tableRow = `
-                <tr>
-                <td>${capitalize(doc.data().category)}</td>
-                <td>${doc.data().amount}</td>
-                <td>${doc.data().description}</td>
+                ${doc.data().type == "Expense" ? '<tr class="table-danger">' : '<tr class="table-primary">'}
+                <td class=${tableClass}>${capitalize(doc.data().category)}</td>
+                <td class=${tableClass}>${doc.data().amount}</td>
+                <td class=${tableClass}>${doc.data().description}</td>
+                <td class=${tableClass}>${(new Date(doc.data().date)).toString().slice(0, 25)}</td>
+                <td class=${tableClass}><button type="button" class="btn btn-primary" id="delete-${doc.id}">${deleteIcon}</button></td>
                 </tr>
                 `;
 
@@ -196,5 +204,5 @@ export const getHistory = async () => {
     }
 }
 
-const historyHeading = document.getElementById("history-heading");
-historyHeading.addEventListener('click', getHistory);
+// const historyHeading = document.getElementById("history-heading");
+// historyHeading.addEventListener('click', getHistory);
